b26419b27da8fd9ae64d55c15d3015ab823c8342
Nathan Edwards <nathan@onedarnleyroad.com>
2019-04-03 12:03:12 +0100

    update seomatic to use trad env settings, to avoid using env(). Fixes #11

--- config/seomatic.php
+++ config/seomatic.php
@@ -1,20 +1,24 @@
 <?php
 
-use function onedarnleyroad\one\env;
-
-// @var $isProduction boolean
-$isProduction = env('ENVIRONMENT') == 'production';
-
 return [
 	// The public-facing name of the plugin
-	'pluginName' => 'SEO',
+	'*' => [
+        'pluginName' => 'SEO',
 
-	// Should the meta generator tag and X-Powered-By header be included?
-    'generatorEnabled' => false,
+        // Should the meta generator tag and X-Powered-By header be included?
+        'generatorEnabled' => false,
 
-    // Should SEOmatic render frontend sitemaps?
-    'sitemapsEnabled' => $isProduction ? true : false,
+        // Should SEOmatic render frontend sitemaps?
+        'sitemapsEnabled' => false,
 
-    // The server environment, either `live`, `staging`, or `local`
-    'environment' => $isProduction ? 'live' : 'local',
+        // The server environment, either `live`, `staging`, or `local`
+        'environment' => 'local',
+    ],
+  
+    // Production environment settings
+    // Avoid env() to "lock down" any setting
+    'production' => [
+        'sitemapsEnabled' => true,
+        'environment' => 'live',
+    ],
 ];
