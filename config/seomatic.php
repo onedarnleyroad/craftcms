7d50a47e10a2f387cdf30dbfd71d78d1d597ff6c
John D Wells <john@onedarnleyroad.com>
2019-05-31 14:57:04 -0500

    Update seomatic.php

--- config/seomatic.php
+++ config/seomatic.php
@@ -8,17 +8,12 @@ return [
         // Should the meta generator tag and X-Powered-By header be included?
         'generatorEnabled' => false,
 
-        // Should SEOmatic render frontend sitemaps?
-        'sitemapsEnabled' => false,
-
         // The server environment, either `live`, `staging`, or `local`
         'environment' => 'local',
     ],
   
     // Production environment settings
-    // Avoid env() to "lock down" any setting
     'production' => [
-        'sitemapsEnabled' => true,
-        'environment' => 'live',
-    ],
+        'environment' => 'live'
+    ]
 ];
