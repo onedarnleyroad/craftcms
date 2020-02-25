5df5c5c1c3e29662c756cae15d1bb4fa16526cd1
John D Wells <john@onedarnleyroad.com>
2020-02-25 15:38:47 -0600

    Clean up postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,51 +1,38 @@
+const autoprefixer = require('autoprefixer')
 
-const cssnano = require("cssnano");
-const purgecss = require('@fullhuman/postcss-purgecss');
-
-class TailwindExtractor {
-	static extract(content) {
-		return content.match(/[A-z0-9-:/]+/g) || [];
+const cssnano = require('cssnano')({
+	preset: 'default',
+	discardUnused: false,
+	discardComments: {
+		removeAll: true
 	}
-}
+})
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: [
+		'./templates/**/*.twig',
+		'./src/**/*.vue'
+	],
+	whitelistPatterns: [
+		/js-/
+	],
+	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+const tailwind = require('tailwindcss')(
+	'./tailwind.config.js'
+)
 
 module.exports = ({ options }) => {
-
-	let plugins = [
-		require('tailwindcss')('./tailwind.config.js'),
-		require('autoprefixer'),
-		purgecss({
-			content: ['./templates/**/*.twig', './src/js/components/**/*.vue', './templates/**/*.html'],
-			extractors: [{
-				extractor: TailwindExtractor,
-				extensions: ["twig", "vue", "html"],
-				whitelistPatterns: [
-					/* 
-						Setup a tailwind whitelist here, or you can do it in HTML
-						but you can use regex here for matching, good for namespaced
-						library CSS like Flickity, eg:
-
-					 	/flickity/,
-				 		/previous/,
-						/next/,
-					*/
-				]
-			}]
-		})
-	];
-
-	if (options.mode === 'production') {
-
-		plugins = [
-			...plugins,
-			cssnano({
-				preset: 'default',
-				discardUnused: false,
-				discardComments: { removeAll: true }
-			})
-		];
-	}
-
 	return {
-		plugins
-	};
-};
+		plugins: [
+			tailwind,
+			autoprefixer,
+			...options.mode === 'production'
+				? [
+					purgecss,
+					cssnano
+				] : []
+		]
+	}
+}
