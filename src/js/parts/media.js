5753cd109fe7fe3a1ffd091e4e9a8006c3a3e88c
John D Wells Jr <john@onedarnleyroad.com>
2020-05-14 16:34:43 -0500

    use tailwind for breakpoints

--- src/js/parts/media.js
+++ src/js/parts/media.js
@@ -1,16 +1,12 @@
-
-
 import { MediaQueries } from "@/libs/media-queries";
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../../tailwind.config.js'
 
-const screens = {
-	mobile: { max: '639px' },
-	sm: { min: '640px' },
-	md: { min: '768px' },
-	lg: { min: '1025px' },
-	xl: { min: '1441px' },
-	'xl2': { min: '1600px' },
-	reducedMotion: `(prefers-reduced-motion: reduce)`,
-};
+const screens = Object.assign(
+  tailwindConfig.theme.screens, {
+    reducedMotion: `(prefers-reduced-motion: reduce)`
+  }
+);
 
 const m = new MediaQueries( screens );
 export const media = m;
