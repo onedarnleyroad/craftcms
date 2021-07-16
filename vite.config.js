cf9fdd6a26162104d8c8d348c9c72f61325b33e9
John D Wells <john@onedarnleyroad.com>
2021-07-16 12:07:09 -0500

    Support CJS and ES5 import syntaxes

--- vite.config.js
+++ vite.config.js
@@ -8,31 +8,34 @@ import path from 'path';
 export default ({ command }) => ({
   base: command === 'serve' ? '' : '/dist/',
   build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
     manifest: true,
-    outDir: path.resolve('./web/dist/'),
+    outDir: path.resolve(__dirname, 'web/dist/'),
     rollupOptions: {
       input: {
-        app: path.resolve('./src/js/app.js'),
+        app: path.resolve(__dirname, 'src/js/app.js'),
       },
     },
   },
   plugins: [
     ViteRestart({
       reload: [
-        path.resolve('./templates/**/*'),
+        path.resolve(__dirname, 'templates/**/*'),
       ],
     }),
   ],
-  publicDir: path.resolve('./src/public'),
+  publicDir: path.resolve(__dirname, 'src/public'),
   resolve: {
     alias: {
-      '@js': path.resolve('./src/js'),
-      '@css': path.resolve('./src/css'),
+      '@css': path.resolve(__dirname, 'src/css'),
+      '@js': path.resolve(__dirname, 'src/js'),
     },
   },
   server: {
     host: '0.0.0.0',
     port: 3000,
-    strictPort: true
-  }
+    strictPort: true,
+  },
 });
