f8c48809b69d6c300d64c95caeb6d019bb7d4cec
John D Wells <john@onedarnleyroad.com>
2021-08-24 15:38:46 -0500

    Comments.

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
-// CSS
+/* CSS */
 import "@js/parts/css";
 
-// Our App
-// eg
+/* JS */
 import "@js/parts/lazyloading";
 
-// Alpine should come last
+/**
+ * Alpine JS
+ * Best to put it last so that all other potential JS is available
+ * when components start getting initialized.
+ */
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
