d4b4b4a241dd9d504569ab9c68976a33fb6cae12
John D Wells <john@onedarnleyroad.com>
2021-08-24 14:09:56 -0500

    Don't ignore ddev, and configure so that npm/vite can run inside the container.

--- config/vite.php
+++ config/vite.php
@@ -3,7 +3,10 @@
 use craft\helpers\App;
 
 return [
-	'devServerPublic' => App::env('VITE_DEV_SERVER_PUBLIC'),
+	'checkDevServer' => true,
+	'devServerInternal' => 'http://localhost:3000',
+	'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':3000',
+	'errorEntry' => 'src/js/app.js',
 	'manifestPath' => '@webroot/dist/manifest.json',
 	'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
 	'useDevServer' => (bool) App::env('VITE_USE_DEV_SERVER'),
