2e38089a455e38219b8bf8646c3d7ebcd3e7dd96
John D Wells <john@onedarnleyroad.com>
2022-04-20 07:38:29 -0500

    Update to Tailwind 3.x

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,22 @@ module.exports = {
   corePlugins: {
     container: false,
   },
-  darkMode: false,
-  mode: 'jit',
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
   ],
-  purge: {
-    content: [
-      './templates/**/*.twig',
-    ],
-  },
+  content: [
+    './templates/**/*.twig',
+  ],
   theme: {
     extend: {
       minHeight: {
-        'screenh' : '100vh'
-      }
+        'screenh' : '100vh',
+      },
     },
     fontFamily: {
       sans: ['Lato', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
-    }
+    },
   },
-  variants: {}
 };
