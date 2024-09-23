b8009ffc2b307005014407ddde0f812aa9dc8f19
Ben Hall <ben@onedarnleyroad.com>
2024-09-23 17:06:48 +0100

    Vite config amends for v5

--- config/vite.php
+++ config/vite.php
@@ -7,7 +7,7 @@ return [
 	'devServerInternal' => 'http://localhost:3000',
 	'devServerPublic' => Craft::getAlias('@web') . ':3000',
 	'errorEntry' => 'src/js/app.js',
-	'manifestPath' => Craft::getAlias('@webroot') . '/dist/manifest.json',
+	'manifestPath' => Craft::getAlias('@webroot') . '/dist/.vite/manifest.json',
 	'serverPublic' => Craft::getAlias('@web')  . '/dist/',
 	'useDevServer' => (bool) App::env('VITE_USE_DEV_SERVER'),
 ];
